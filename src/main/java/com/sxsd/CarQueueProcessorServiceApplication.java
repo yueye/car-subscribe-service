ccb394355a41bd9dba9a0fcba31b6e891096a184
liujx15 <liujx15@lenovo.com>
2019-05-23 14:23:14 +0800

    提交测试stash

--- src/main/java/com/sxsd/CarQueueProcessorServiceApplication.java
+++ src/main/java/com/sxsd/CarQueueProcessorServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 入口
+ * 入口1
  */
 @SpringBootApplication
 public class CarQueueProcessorServiceApplication {
